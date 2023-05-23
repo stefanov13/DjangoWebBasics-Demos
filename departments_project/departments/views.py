c985859259e1db9b1208593180028643450ccd4c
chavdar tonchev <106109451+chavdar94@users.noreply.github.com>
2023-05-23 04:25:44 +0300

    Update views.py

--- departments_project/departments/views.py
+++ departments_project/departments/views.py
@@ -6,7 +6,7 @@ from .forms import TestForm, ModelTestForm
 
 
 def index(request):
-    # Взема всичко обекти от базата
+    # Взема всички обекти от базата
     users = Person.objects.all()
 
     context = {
